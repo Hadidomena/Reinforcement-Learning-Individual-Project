4282b1a7d3373ba6d98d52624d9666ba366c5678
szep <szepiet33@gmail.com>
2025-03-19 14:14:47 +0100

    implemented Snake with two versions, one for the human player and the second for the AI used in RL, based on the tutorial

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import pygame
+from snake import SnakeGame
+
 def main():
-    print("Hello World!")
+    pygame.init()
+    game = SnakeGame()
+    clock = pygame.time.Clock()
+    running = True
+    
+    # game loop
+    while running:
+
+        game_over, score = game.play_step()
+        
+        if game_over == True:
+            break
+        
+        clock.tick(15)
+        
+    print('Final Score', score)
+        
+        
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
